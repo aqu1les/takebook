230fb13fded8885932d6164cc080581f386f0005
Felipe Barros <felipebarros.dev@gmail.com>
2020-04-29 22:53:07 -0300

    updated texts there are used

--- src/languages/en-US.js
+++ src/languages/en-US.js
@@ -4,6 +4,13 @@ export default {
             welcome: 'Welcome to Takebook',
         },
 
+        error: {
+            noConnection: "We're unable to contact the server",
+            invalidEmail: 'Invalid email',
+            wrongPassword: 'Wrong password',
+            somethingWentWrong: 'Something went wrong, try again later',
+        },
+
         login: {
             email: 'E-mail',
             password: 'Password',
@@ -13,11 +20,79 @@ export default {
             register: 'Register',
         },
 
-        error: {
-            no_connection: "We're unable to contact the server",
-            invalid_email: 'Invalid email',
-            wrong_password: 'Wrong password',
-            something_went_wrong: 'Something went wrong, try again later',
+        signUp: {
+            name: 'Name',
+            email: 'Your best e-mail',
+            password: 'Type your password',
+            confirmPassword: 'Confirm your password',
+            register: 'Register',
+
+            success: {
+                title: 'Registered successfully!',
+                content: 'Feel free to publish or purchase new books!',
+                button: 'Begin',
+            },
+        },
+
+        advertList: {
+            recent: 'Most Recent',
+            noBooks: 'No books registered!',
+            advert: {
+                locale: 'Locale',
+                new: 'New',
+                semi: 'Semi-New',
+                used: 'Used',
+            },
+        },
+
+        advertDetails: {
+            tabs: {
+                description: 'Description',
+                localization: 'Localization',
+                contact: 'Contact',
+            },
+            profile: {
+                button: 'See Profile',
+                talk: 'Get in touch',
+            },
+        },
+
+        categories: {
+            highlights: 'Highlights',
+        },
+
+        onboardModals: {
+            first: {
+                header: 'Ready to publish your book?',
+                content: "It's very simple, there are just 2 steps!",
+                button: "Let's go",
+            },
+            second: {
+                textplanation:
+                    'You will need a photo of the cover and back cover. Use your best techniques as a photographer to make your ad more attractive: D',
+            },
+        },
+
+        newBook: {
+            pageOne: {
+                help1: 'Click on the fields to add your pictures',
+                help2: '* Drag to the left to add more pictures to your ad',
+            },
+            pageTwo: {
+                help1: 'Tell me more about your book',
+                remember: 'Remember',
+                help2:
+                    'A well-described book is more easily found in a search.',
+                title: 'What is the title of your book?',
+                author: 'What about the author?',
+                bookStatus: "Set your book's condition",
+                howMuch: 'How much?',
+            },
+            pageThree: {
+                categories: 'Select the categories your book fits',
+                description: 'Describe your book, the more details the better!',
+                button: 'Publish',
+            },
         },
     },
 };
--- src/languages/pt-BR.js
+++ src/languages/pt-BR.js
@@ -4,6 +4,14 @@ export default {
             welcome: 'Bem vindo ao Takebook !',
         },
 
+        error: {
+            noConnection: 'Não foi possível contactar o servidor!',
+            invalidEmail: 'Email inválido',
+            wrongPassword: 'Senha errada',
+            somethingWentWrong:
+                'Algo de errado aconteceu, tente novamente mais tarde.',
+        },
+
         login: {
             email: 'Digite aqui o seu e-mail',
             password: 'Digite aqui a sua senha',
@@ -13,12 +21,82 @@ export default {
             register: 'Cadastre-se',
         },
 
-        error: {
-            no_connection: 'Não foi possível contactar o servidor!',
-            invalid_email: 'Email inválido',
-            wrong_password: 'Senha errada',
-            something_went_wrong:
-                'Algo de errado aconteceu, tente novamente mais tarde.',
+        signUp: {
+            name: 'Nome',
+            email: 'Digite seu melhor e-mail',
+            password: 'Digite sua senha',
+            confirmPassword: 'Digite sua senha novamente',
+            register: 'Cadastrar',
+
+            success: {
+                title: 'Cadastro realizado!',
+                content:
+                    'Sinta-se à vontade para publicar um livro ou adquirir novos!',
+                button: 'Começar',
+            },
+        },
+
+        advertList: {
+            recent: 'Mais Recentes',
+            noBooks: 'Nenhum livro foi cadastrado!',
+            advert: {
+                locale: 'Local',
+                new: 'Novo',
+                semi: 'Semi-Novo',
+                used: 'Usado',
+            },
+        },
+
+        advertDetails: {
+            tabs: {
+                description: 'Descrição',
+                localization: 'Localização',
+                contact: 'Contato',
+            },
+            profile: {
+                button: 'Ver Perfil',
+                talk: 'Entrar em contato',
+            },
+        },
+
+        categories: {
+            highlights: 'Destaques',
+        },
+
+        onboardModals: {
+            first: {
+                header: 'Você está pronto para anunciar o seu livro?',
+                content: 'É muito simples, são apenas 2 passos!',
+                button: 'Vamos lá',
+            },
+            second: {
+                textplanation:
+                    'Será necessário a foto da capa e contracapa. Utilize suas melhores técnicas como fotógrafo para o seu anúncio ficar mais atraente :D',
+            },
+        },
+
+        newBook: {
+            pageOne: {
+                help1: 'Clique nos campos para as adicionar fotos desejadas',
+                help2:
+                    '* Arraste para esquerda caso queira adicionar mais fotos para o seu anúncio',
+            },
+            pageTwo: {
+                help1: 'Me fale sobre seu livro...',
+                remember: 'Lembre-se',
+                help2:
+                    'Um livro bem descrito é mais facilmente encontrado numa pesquisa.',
+                title: 'Qual o título do livro?',
+                author: 'E o autor?',
+                bookStatus: 'Defina a condição do livro',
+                howMuch: 'Quanto quer por ele?',
+            },
+            pageThree: {
+                categories: 'Em quais categorias ele se encaixa?',
+                description:
+                    'Descreva bem o seu livro, quanto mais detalhes melhor!',
+                button: 'Publicar',
+            },
         },
     },
 };
