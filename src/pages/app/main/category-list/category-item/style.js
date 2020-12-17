86c94dae453d1c745863b6cb9985e980727dbb03
Felipe Barros <felipebarros.dev@gmail.com>
2020-12-16 22:42:40 -0300

    fix(category-list): item layout

--- src/pages/app/main/category-list/category-item/style.js
+++ src/pages/app/main/category-list/category-item/style.js
@@ -3,9 +3,11 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
 	Category: {
 		height: '100%',
-		width: 70,
+		width: 78,
 		flexDirection: 'column',
 		justifyContent: 'center',
 		alignItems: 'center',
+		borderRadius: 8,
+		paddingHorizontal: 4,
 	},
 });
